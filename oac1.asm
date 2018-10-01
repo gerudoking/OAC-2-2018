bb766759f43b69d652e68eb46e7ad3bc4865db59
gerudoking <edsantos2003@gmail.com>
2018-10-01 15:06:41 -0300

    Começo da escolha de rotinas e edge extraction

--- oac1.asm
+++ oac1.asm
@@ -8,8 +8,12 @@
 imageBuffer:	.space	1000000
 inputBuffer:	.space	1000000
 file:		.asciiz	"img.bmp"
-blurMessage:	.asciiz "Digite a altura da curva gaussiana"
+chooseMessage:	.asciiz "Digite qual operacao deseja executar(1 para blur, 2 para edge extraction, 3 para threshold)"
+blurMessage:	.asciiz "Digite o tamanho da mascara"
 blurMask:	.space	1000000
+edgeMessage:	.asciiz	"Digite o tamanho da laplaciana"
+edgeMask:	.space	1000000
+mask:		.space	1000000
 
 .text
 openfile:
@@ -49,11 +53,59 @@ j loop
 
 exit:
 
-getBlurMaskParameters:	#Janela de diálogo que pega um inteiro para mascara do blur
+clean:
+move $t0, $zero
+move $t1, $zero
+move $t2, $zero
+move $t3, $zero
+move $t4, $zero
+move $t5, $zero
+move $t6, $zero
+move $t7, $zero
+
+chooseOperation:	#Escolhe a operação a ser executada na imagem
+li $v0, 51
+la $a0, chooseMessage
+syscall
+move $s0, $a0
+beq $s0, 1, getBlurMaskParameters
+beq $s0, 2, getEdgeMaskParameters
+
+getEdgeMaskParameters:	#Pega um número para construir a matriz laplaciana que servirá de máscara. Precisa ser ímpar!
+li $v0, 51
+la $a0, edgeMessage
+syscall
+move $s7, $a0
+la $t0, edgeMask
+move $t1, $zero
+mul $t2, $s7, $s7
+div $t3, $t2, 2
+addi $t3, $t3, 1
+#la $t4, mask
+
+edgeMaskCreate:
+li $t5, -1
+bne $t1, $t3, continueEdgeMaskCreate
+move $t5, $t2
+subi $t5, $t5, 1
+continueEdgeMaskCreate:
+mul $t4, $t1, 4
+sw $t5, mask($t4)
+addi $t1, $t1, 1
+beq $t1, $t2, edgeMaskLoop
+j edgeMaskCreate
+
+edgeMaskLoop:
+
+getBlurMaskParameters:	#Janela de diálogo que pega um inteiro para mascara do blur. Precisa ser impar!
 li $v0, 51
 la $a0, blurMessage
 syscall
 move $s7, $a0
 la $t0, blurMask
 
+blurMaskCreate:
+
 blurMaskLoop:		#Constroi a máscara
+
+finalExit:
